b4c7cfb26cb71fe62cf3252332d7dfdd70c855b3
ozyb <1850559684@qq.com>
2020-12-25 23:24:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,7 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# 增加Kenzok8的软件库 包含passwall及其依赖
+echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
